1ac2457826bd46436647d31e039c14f9a2f50773
videsh_hedge <videsh_hedge@persistent.com>
2021-05-07 21:37:10 +0530

    basic image upload added

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from  'multer';
+import { extname } from  'path';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -13,5 +16,22 @@ export class TasksController {
     @UsePipes(ValidationPipe)
     async createTask(@Body() createTaskDto: CreateTaskDto): Promise<any> {
         return this.tasksService.createTask(createTaskDto);
+    }
+
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('media',
+      {
+        storage: diskStorage({
+          destination: './uploads', 
+          filename: (req, file, cb) => {
+          const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
+          return cb(null, `${randomName}${extname(file.originalname)}`)
+        }
+        })
+      }
+    )
+    )
+    uploadFile(@UploadedFile() media: Express.Multer.File, @Body() createTaskDto: CreateTaskDto) {
+    console.log(media, createTaskDto);
     }
 }
