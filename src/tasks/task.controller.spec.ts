6bda81c340ca3a7b534185d78195cb55d8c08913
videsh_hedge <videsh_hedge@persistent.com>
2021-05-08 23:50:29 +0530

    updated test cases

--- src/tasks/task.controller.spec.ts
+++ src/tasks/task.controller.spec.ts
@@ -12,7 +12,8 @@ const mockTasksService = () => ({
     getTaskById: jest.fn(),
     getAllTasks: jest.fn(), 
     getTasksWithFilters: jest.fn(),
-    deleteTaskById: jest.fn(),   
+    deleteTaskById: jest.fn(),
+    updateTask: jest.fn(),   
 });
 
 describe('TasksController', () => {
@@ -127,6 +128,66 @@ describe('TasksController', () => {
             await taskController.deleteTaskById(mTaskId);
             expect(taskService.deleteTaskById).toHaveBeenCalled();
             expect(taskService.deleteTaskById).toBeCalledTimes(1);
+        });
+    });
+
+    describe('Test update a task method', () => {
+        it('should Test update task method of TasksController', async () => {
+            // Add mock file for create task
+            const fileToBuffer = (filename) => {
+                const readStream = fs.createReadStream(filename);
+                const chunks = [];
+                return new Promise((resolve, reject) => {
+                  // Handle any errors while reading
+                  readStream.on('error', (err) => {
+                    // handle error
+                    // File could not be read
+                    reject(err);
+                  });
+              
+                  // Listen for data
+                  readStream.on('data', (chunk) => {
+                    chunks.push(chunk);
+                  });
+              
+                  // File is done being read
+                  readStream.on('close', () => {
+                    // Create a buffer of the image from the stream
+                    resolve(Buffer.concat(chunks));
+                  });
+                });
+            };
+
+            const imageBuffer = (await fileToBuffer('./uploads/testImage.png')) as Buffer;
+            let media: Express.Multer.File;
+            const myReadableStreamBuffer = new streamBuffers.ReadableStreamBuffer({
+            frequency: 10, // in milliseconds.
+            chunkSize: 2048, // in bytes.
+            });
+            myReadableStreamBuffer.put(imageBuffer as Buffer);
+            media = {
+                buffer: imageBuffer,
+                fieldname: 'fieldname-defined-in-@UseInterceptors-decorator',
+                originalname: 'original-filename',
+                encoding: '7bit',
+                mimetype: 'file-mimetyp',
+                destination: 'destination-path',
+                filename: 'file-name',
+                path: 'file-path',
+                size: 955578,
+                stream: myReadableStreamBuffer,
+            };
+
+            const mTaskData = {
+                title: 'Task 1',
+                description: 'Short description',
+                target_date: '2021-05-08T03:27:21.575Z',
+                status: 'In Progress',
+            };
+            const taskId = '609670a9e7799fb0704d974b'
+            await taskController.updateTask(media, taskId, mTaskData);
+            expect(taskService.updateTask).toHaveBeenCalled();
+            expect(taskService.updateTask).toBeCalledTimes(1);
         });
     });
 
